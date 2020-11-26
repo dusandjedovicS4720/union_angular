2c49ea6b0de89ea667b00de2ff16bef11ff4400a
ddjedovic <djedovicdusan94@protonmail.com>
2020-11-26 20:18:41 +0100

    Poslednja radna verzija 20:18

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      margin: {
+        // top right bottom left
+        "30%": "0 30% 0 30%",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        blue: {
+          1000: "#1C1B7E",
+        },
+        purple: {
+          1000: "#8C4BFF",
+        },
+        yellow: {
+          1000: "#FDE895",
+        },
+      },
+      zIndex: {
+        "-10": "-10",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
